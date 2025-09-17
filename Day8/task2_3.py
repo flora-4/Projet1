ca7f6a59d6b40dd97128ffc80eb4a7fc4d07f165
flora-4 <flora-dolorece.djouela-tabeu@epitech.eu>
2025-09-17 15:51:49 +0200

    firt turtle

--- Day8/task2_3.py
+++ Day8/task2_3.py
@@ -1,2 +1,19 @@
 import turtle
 
+def draw_polygon(sides):
+    if sides < 3:
+        print("Un polygone a au moins 3 côtés.")
+        return
+
+    angle = 360 / sides
+    for _ in range(sides):
+        pen.forward(100)
+        pen.right(angle)
+
+screen = turtle.Screen()
+pen = turtle.Turtle()
+
+draw_polygon(5)
+
+screen.mainloop()
+
