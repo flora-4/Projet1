101f01d702aadd8c3898c9f2ba13fd8b065a73d0
flora-4 <flora-dolorece.djouela-tabeu@epitech.eu>
2025-09-19 09:40:59 +0200

    WIP: backup all changes before rebase

--- Day7/script.py
+++ Day7/script.py
@@ -1,7 +1,7 @@
 import random
 from english_words import english_words_lower_set
 
-random_word = random.choice(list(english_words_lower_set))
+print(list(english_words_lower_set))
 
-print(random_word)
+#print(random_word)
 
--- Day7/venv/bin/activate.fish
+++ Day7/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/dolorece/Day7/venv
+set -gx VIRTUAL_ENV /home/dolorece/Projet1/Day7/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
